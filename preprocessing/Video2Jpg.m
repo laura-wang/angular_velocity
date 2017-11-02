745cc95a13c6ab42f97fbf0274f19a4fbae38866
laura-wang <jiangliuwang1994@gmail.com>
2017-11-02 11:22:40 +0800

    Update Video2Jpg.m

--- preprocessing/Video2Jpg.m
+++ preprocessing/Video2Jpg.m
@@ -1,62 +1,46 @@
 clc;
 clear;
-vid_path='D:\WANG Jiangliu\ROSE data\RGB\nturgbd_rgb_s001\nturgb+d_rgb';
-%addpath(vid_path);
-all_vid_path=fullfile(vid_path,'\*.avi');
-vid_name_list=dir(all_vid_path);
-vid_num=length(vid_name_list);
-tic;
-for i=1:1
-    fprintf('%s\n',vid_name_list(i).name);
-    %fprintf('%s\n',vid_name_list(i).name(1:end-4));
-    dir_name=fullfile(vid_path,vid_name_list(i).name(1:end-4));
-    mkdir(dir_name);
-    
-    video=VideoReader(vid_name_list(i).name);
-    frame_number=floor(video.Duration * video.FrameRate);
-    
-    for i=1:frame_number
-        image_name=fullfile(dir_name,'\',num2str(i));
-        image_name=strcat(image_name,'.jpg');
-        I=read(video,i); 
-        %I=gpuArray(I);
-        imwrite(I,image_name,'jpg');                   
-        I=[];
-    end  
-    
+close all;
+video=VideoReader('data\M_00227.avi');
+frame_number=floor(video.Duration * video.FrameRate);
+dir_name=video.name(1:end-4); mkdir(dir_name);
+
+for i=1:frame_number
+    image_name=fullfile(dir_name,'\',num2str(i));
+    image_name=strcat(image_name,'.png');
+    I=read(video,i); 
+    imwrite(I,image_name,'png');                   
+    I=[];
+
 end
-toc;
-% while ~feof(name_list)
-%     vid_name=fgetl(name_list);
-%     fprintf('%s\n',vid_name);
-%     fprintf('%s\n',vid_name(1:end-4));
+
+
+
+% vid_path='D:\WANG Jiangliu\Research\methodology\orientation\code\compute_angular_velocity\preprocessing';
+% %addpath(vid_path);
+% all_vid_path=fullfile(vid_path,'\*.avi');
+% vid_name_list=dir(all_vid_path);
+% vid_num=length(vid_name_list);
+% tic;
+% for i=1:1
+%     fprintf('%s\n',vid_name_list(i).name);
+%     %fprintf('%s\n',vid_name_list(i).name(1:end-4));
+%     dir_name=fullfile(vid_path,vid_name_list(i).name(1:end-4));
+%     mkdir(dir_name);
 %     
-%     %video=VideoReader(vid_name);
-%     %frame_number=floor(video.Duration * video.FrameRate);
+%     video=VideoReader(vid_name_list(i).name);
+%     frame_number=floor(video.Duration * video.FrameRate);
 %     
-% %     %for i=1:frame_number
-% %     %   image_name=strcat('video3sup\',num2str(i));
-% %        image_name=strcat(image_name,'.jpg');
-% %        I=read(video,i);                              
-% %        imwrite(I,image_name,'jpg');                   
-% %        I=[];
-% %     end  
-% %     num=num+1;
+%     for i=1:frame_number
+%         image_name=fullfile(dir_name,'\',num2str(i));
+%         image_name=strcat(image_name,'.jpg');
+%         I=read(video,i); 
+%         %I=gpuArray(I);
+%         imwrite(I,image_name,'jpg');                   
+%         I=[];
+%     end  
+%     
 % end
-% fprintf('%d\n',num);
-% fclose(name_list);
-% 
-%     
-% 
-% video_file='S001C001P001R001A001_rgb.avi';
-% video=VideoReader(video_file);
-% frame_number=floor(video.Duration * video.FrameRate);
-% 
-% 
-% for i=1:frame_number
-%     image_name=strcat('video3sup\',num2str(i));
-%     image_name=strcat(image_name,'.jpg');
-%     I=read(video,i);                               %????
-%     imwrite(I,image_name,'jpg');                   %???
-%     I=[];
-% end
+% toc;
+
+
